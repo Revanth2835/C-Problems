7b6b82fdc7b49550366e7e87f8ad93519c981c5b
Revanth2835 <revanthkumar2835@gmail.com>
2026-02-03 22:42:53 +0530

    code changes in selection sort

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
  using namespace std;
 
+// Time Complexity --> O(n);
+// Space Complexity ==> O(n);
 
  void selectionSort(int arr[] , int n){
     for(int i=0;i<n-1;i++){
@@ -20,8 +22,13 @@
  int main(){
 
     int arr[] = {2,343,4534,12,0};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    selectionSort(arr,n);
+
+     int n = sizeof(arr) / sizeof(arr[0]);
+
+    int arr2[] =  copy(arr,arr+n,arr2); //   array is considered as a pointer in functions :  (&arr[0] or arr  - start index ,  arr+1 -- stop index , arr2) -- where to copy
+
+   
+    selectionSort(arr2,n);
     for(int i =0;i<n;i++){
         cout << arr[i] << " ";
     }
